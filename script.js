a494792d618501a50bc9184e0336aa38d289498d
josephndegwa <kariukindegwa98@gmail.com>
2022-07-20 13:09:56 +0300

    update to street view

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var map;
 require(["esri/map","dojo/domReady!"],function (Map) {
     map = new Map("map", {
-        basemap: "topo-vector",
+        basemap: "streets",
         center: [36.9,-1.22],
         zoom: 11
       });
