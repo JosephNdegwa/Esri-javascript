186f52b8fb28cc3812323b3a693c4ed2239494e6
josephndegwa <kariukindegwa98@gmail.com>
2022-07-21 17:16:34 +0300

    add search api

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-var map;
+/*var map;
 require(["esri/map","dojo/domReady!"],function (Map) {
-    map = new Map("map", {
+    map = new Map("mapDiv", {
         basemap: "streets",
         center: [36.9,-1.22],
         zoom: 11
       });
 });
+*/
+
+require([
+
+  "esri/map",
+  "esri/dijit/Search",
+  "dojo/domReady!"
+
+], function (Map, Search) {
+   var map = new Map("map", {
+      basemap: "gray-vector",
+      center: [36.9,-1.22], // lon, lat
+      zoom: 6
+   });
+
+   var search = new Search({
+      map: map
+   }, "search");
+   search.startup();
+
+});
+
